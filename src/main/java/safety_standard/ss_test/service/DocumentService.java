3cf9e33b53fdd8c86349e3d92ff2b01792d8209d
Nearbel <alishuk0510@mail.ru>
2024-06-28 16:54:16 +0300

    Исправлен подсчет документов по категориям, обработано закрытие транзакции в случае ошибки

--- src/main/java/safety_standard/ss_test/service/DocumentService.java
+++ src/main/java/safety_standard/ss_test/service/DocumentService.java
@@ -20,200 +20,165 @@ public class DocumentService {
         .addAnnotatedClass(DocumentEntity.class)
         .buildSessionFactory();
 
-    public void saveDoc(DocumentEntity document) {
-        Session session = factory.getCurrentSession();
-        try {
-            session.beginTransaction();
-            session.merge(document);
-            session.getTransaction().commit();
-        }
-        finally {
-            session.close();
-        }
-    }
-
-    public void saveListOfDoc(List<DocumentEntity> listOfDoc) {
-        Session session = factory.getCurrentSession();
-        try {
-            session.beginTransaction();
-
-            for(DocumentEntity document: listOfDoc) {
-                session.merge(document);
-                session.getTransaction().commit();
-            }
-        }
-        finally {
-            session.close();
-        }
-    }
-
     public void saveListOfDocWithCheckExists(List<DocumentEntity> listOfDoc) {
-        Session session = factory.getCurrentSession();
-        try {
-            session.beginTransaction();
-            Long id = null;
+        try ( Session session = factory.getCurrentSession() ) {
+            try {
+                session.beginTransaction();
+                Long id = null;
 
-            // обновляем все записи на текущую дату, у которых дата удаления равна null
-            String updateHql = "update DocumentEntity d " +
-                    "set d.dateDelete = :currentDate " +
-                    "where d.dateDelete is null";
-            session.createQuery(updateHql).setParameter("currentDate", Instant.now()).executeUpdate();
+                // обновляем все записи на текущую дату, у которых дата удаления равна null
+                String updateHql = "update DocumentEntity d " +
+                        "set d.dateDelete = :currentDate " +
+                        "where d.dateDelete is null";
+                session.createQuery(updateHql).setParameter("currentDate", Instant.now()).executeUpdate();
 
-            for(DocumentEntity document: listOfDoc) {
-                // проверяем существование документа в БД
-                String checkHql = "select id " +
-                        "from DocumentEntity d " +
-                        "where d.docName = :name and d.dateCreate = :dateCreate and d.dataArticleId = :dataArticleId";
-                Query<Long> query = session.createQuery(checkHql, Long.class);
-                query.setParameter("name", document.getDocName());
-                query.setParameter("dateCreate", document.getDateCreate());
-                query.setParameter("dataArticleId", document.getDataArticleId());
-                id = query.getSingleResultOrNull();
+                for (DocumentEntity document : listOfDoc) {
+                    // проверяем существование документа в БД
+                    String checkHql = "select id " +
+                            "from DocumentEntity d " +
+                            "where d.docName = :name and d.dateCreate = :dateCreate and d.dataArticleId = :dataArticleId";
+                    Query<Long> query = session.createQuery(checkHql, Long.class);
+                    query.setParameter("name", document.getDocName());
+                    query.setParameter("dateCreate", document.getDateCreate());
+                    query.setParameter("dataArticleId", document.getDataArticleId());
+                    id = query.getSingleResultOrNull();
 
-                if(id == null) {
-                    session.merge(document);
+                    if (id == null) {
+                        session.merge(document);
+                    } else {
+                        // Если запись существует, обновляем дату удаления обратно на null
+                        String resetHql = "update DocumentEntity d " +
+                                "set d.dateDelete = NULL " +
+                                "where d.id = :id";
+                        session.createQuery(resetHql).setParameter("id", id).executeUpdate();
+                    }
                 }
-                else {
-                    // Если запись существует, обновляем дату удаления обратно на null
-                    String resetHql = "update DocumentEntity d " +
-                            "set d.dateDelete = NULL " +
-                            "where d.id = :id";
-                    session.createQuery(resetHql).setParameter("id", id).executeUpdate();
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                if (session.getTransaction().isActive() && session != null && session.getTransaction() != null) {
+                    session.getTransaction().rollback();
                 }
+                e.printStackTrace();
             }
-            session.getTransaction().commit();
         }
-        finally {
-            session.close();
-        }
-    }
-
-    public List<ConstantEntity> getAllConst() {
-        Session session = factory.getCurrentSession();
-        List<ConstantEntity> url = new ArrayList<>();
-
-        try {
-            session.beginTransaction();
-
-            Query<ConstantEntity> query = session.createQuery("from ConstantEntity", ConstantEntity.class);
-            url = query.getResultList();
-
-            session.getTransaction().commit();
-        }
-        finally {
-            session.close();
-        }
-
-        return url;
     }
 
     public String getUrl() {
-        Session session = factory.getCurrentSession();
         String url = "";
 
-        try {
-            session.beginTransaction();
+        try (Session session = factory.getCurrentSession()) {
+            try {
+                session.beginTransaction();
 
-            Query<String> query = session.createQuery("select value from ConstantEntity where name='URL'");
-            url = query.uniqueResult();
+                Query<String> query = session.createQuery("select value from ConstantEntity where name='URL'");
+                url = query.uniqueResult();
 
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            if (session.getTransaction().isActive()) {
-                session.getTransaction().rollback();
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                if (session.getTransaction().isActive() && session != null && session.getTransaction() != null) {
+                    session.getTransaction().rollback();
+                }
+                e.printStackTrace();
             }
-            e.printStackTrace();
         }
-        finally {
-            session.close();
-        }
-
         return url;
     }
 
     public String getPath() {
-        Session session = factory.getCurrentSession();
-        String path;
+        String path = "";
 
-        try {
-            session.beginTransaction();
-            Query<String> query = session.createQuery("select value from ConstantEntity where name='PATH'");
-            path = query.uniqueResult();
-            session.getTransaction().commit();
-        }
-        finally {
-            session.close();
+        try (Session session = factory.getCurrentSession()) {
+            try {
+                session.beginTransaction();
+                Query<String> query = session.createQuery("select value from ConstantEntity where name='PATH'");
+                path = query.uniqueResult();
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                if (session.getTransaction().isActive() && session != null && session.getTransaction() != null) {
+                    session.getTransaction().rollback();
+                }
+                e.printStackTrace();
+            }
         }
 
         return path;
     }
 
     public void saveParameters(String URL, String PATH) {
-        Session session = factory.getCurrentSession();
-        try {
-            session.beginTransaction();
+        try (Session session = factory.getCurrentSession()) {
+            try {
+                session.beginTransaction();
 
-            String urlHql = "update ConstantEntity " +
-                    "set value = :URL " +
-                    "where name='URL'";
-            session.createQuery(urlHql).setParameter("URL", URL).executeUpdate();
+                String urlHql = "update ConstantEntity " +
+                        "set value = :URL " +
+                        "where name='URL'";
+                session.createQuery(urlHql).setParameter("URL", URL).executeUpdate();
 
-            String pathHql = "update ConstantEntity " +
-                    "set value = :PATH " +
-                    "where name='PATH'";
-            session.createQuery(pathHql).setParameter("PATH", PATH).executeUpdate();
+                String pathHql = "update ConstantEntity " +
+                        "set value = :PATH " +
+                        "where name='PATH'";
+                session.createQuery(pathHql).setParameter("PATH", PATH).executeUpdate();
 
-            session.getTransaction().commit();
-        }
-        finally {
-            session.close();
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                if (session.getTransaction().isActive() && session != null && session.getTransaction() != null) {
+                    session.getTransaction().rollback();
+                }
+                e.printStackTrace();
+            }
         }
     }
 
     public List<Category> category() {
-        Session session = factory.getCurrentSession();
         List<Category> categoryList = new ArrayList<>();
 
-        try {
-            session.beginTransaction();
+        try (Session session = factory.getCurrentSession()) {
+            try {
+                session.beginTransaction();
 
-            String hql = "select new safety_standard.ss_test.dto.Category(category, count(category)) " +
-                    "from DocumentEntity " +
-                    "group by category " +
-                    "order by category";
-            Query<Category> query = session.createQuery(hql, Category.class);
-            categoryList = query.getResultList();
+                String hql = "select new safety_standard.ss_test.dto.Category(category, count(case when dateDelete is null then 1 end)) " +
+                        "from DocumentEntity " +
+                        "group by category " +
+                        "order by category";
+                Query<Category> query = session.createQuery(hql, Category.class);
+                categoryList = query.getResultList();
 
-            session.getTransaction().commit();
-        }
-        finally {
-            session.close();
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                if (session.getTransaction().isActive() && session != null && session.getTransaction() != null) {
+                    session.getTransaction().rollback();
+                }
+                e.printStackTrace();
+            }
         }
         return categoryList;
     }
 
     public List<Category> categoryWithAddition(Instant dateCSVFile) {
-        Session session = factory.getCurrentSession();
         List<Category> categoryList = new ArrayList<>();
 
-        try {
-            session.beginTransaction();
+        try (Session session = factory.getCurrentSession()) {
+            try {
+                session.beginTransaction();
 
-            String hql = "select new safety_standard.ss_test.dto.Category(" +
-                            "category, " +
-                            "count(category), " +
-                            "count(case when :dateCSVFile < dateCreate and dateDelete is null then 1 end), " +
-                            "count(case when :dateCSVFile < dateDelete then 1 end)) " +
-                    "from DocumentEntity " +
-                    "group by category " +
-                    "order by category";
-            Query<Category> query = session.createQuery(hql, Category.class).setParameter("dateCSVFile", dateCSVFile);
-            categoryList = query.getResultList();
+                String hql = "select new safety_standard.ss_test.dto.Category(" +
+                        "category, " +
+                        "count(case when dateDelete is null then 1 end), " +
+                        "count(case when :dateCSVFile < dateCreate and dateDelete is null then 1 end), " +
+                        "count(case when :dateCSVFile < dateDelete then 1 end)) " +
+                        "from DocumentEntity " +
+                        "group by category " +
+                        "order by category";
+                Query<Category> query = session.createQuery(hql, Category.class).setParameter("dateCSVFile", dateCSVFile);
+                categoryList = query.getResultList();
 
-            session.getTransaction().commit();
-        }
-        finally {
-            session.close();
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                if (session.getTransaction().isActive() && session != null && session.getTransaction() != null) {
+                    session.getTransaction().rollback();
+                }
+                e.printStackTrace();
+            }
         }
         return categoryList;
     }
